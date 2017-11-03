a638039c381d342462d33f77c6aefdd4d88e337c
rumblingturtle <edzaljaev@gmail.com>
2017-11-03 23:28:38 +0300

    Я завез файлы
    И пиздатые комменты

--- __main__.py
+++ __main__.py
@@ -2,20 +2,43 @@ from twisted.internet.protocol import Protocol, Factory
 from twisted.internet import reactor
 import json
 
+
 class MessengerProtocol(Protocol):
+
 	def connectionMade(self):
+		self.currentMessage = ""
 		self.factory.clients.append(self)
 
 	def connectionLost(self, reason):
 		self.factory.clients.remove(self)
 
 	def dataReceived(self, data):
-		print data
-		message = json.loads(j)
-		if(message.type=="Text"):
-			for c in self.factory.clients:
-				c.transport.write(data)
-
+		if data[len(data)-3:]=="END":
+			print self.currentMessage+data[:len(data)-3]
+			message = json.loads(self.currentMessage+data[:len(data)-3])
+			if(message['type']=="Table Request"):
+				print "idi naxui" #fetch and send table of files
+			if(message['type']=="File Request"):
+				file ="" #fetch file and convert to base 64
+				request = {"type":"File","name":"","filename":message["filename"],"date":"","text":file}
+				self.transport.write(json.dumps(request))
+			if(message['type']=="File"):
+				#write file on server (todo: sdelat eto normalno)
+				#file = open("files\\"+message["filename"],'w')
+				#file.write(message["text"])
+				#file.close()
+				notification = {"type":"Notification","name":"",\
+				"filename":"","date":"","text":message["date"] +\
+				 "  User " + message["name"] + " uploaded new file " + message["filename"]+"\n"}
+				for c in self.factory.clients:
+					c.transport.write(json.dumps(notification))
+				self.currentMessage=""
+			if(message['type']=="Text"):
+				for c in self.factory.clients:
+					c.transport.write(self.currentMessage)
+				self.currentMessage=""
+		else:
+			self.currentMessage+=data
 
 if __name__ == "__main__":
 	factory = Factory()
